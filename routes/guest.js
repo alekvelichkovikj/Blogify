d047c166b5a10a923136df2795c9b0db70db888c
Aleksandar Velichkovikj <al.velichkovikj@gmail.com>
2021-11-24 18:02:31 +0100

    finished styles

--- routes/guest.js
+++ routes/guest.js
@@ -1,26 +1,25 @@
 const router = require('express').Router()
-const Editor = require('../models/Editor');
-const Post = require('../models/Post');
+const Editor = require('../models/Editor')
+const Post = require('../models/Post')
 
-router.get("/guest/guestView",  (req, res, next) => {
-    const {title, content, editorId} = req.body
-    Post.find()
-    .then(postFromDB => {
-        res.render('guest/guestView', { post: postFromDB[Math.floor(Math.random()  * postFromDB.length)] })
+router.get('/guest/guestView', (req, res, next) => {
+  // const { title, content, editorId } = req.body
+  Post.find()
+    .populate('editorId')
+    .then((postFromDB) => {
+      res.render('guest/guestView', {
+        post: postFromDB[Math.floor(Math.random() * postFromDB.length)],
+      })
     })
-    // res.render('guest/guestView');
-  });
+  // res.render('guest/guestView');
+})
 
 // router.get('/guest/randomPost', (req, res, next) => {
 //     const { title, content, editorId} = req.body
 //     Post.find()
-//     .then(postFromDB =>{     
+//     .then(postFromDB =>{
 //      res.render('guest/randomPost', { post: postFromDB[Math.floor(Math.random()  * postFromDB.length)] })
 //     })
 // })
-
-
-
-
 
 module.exports = router
